1ecac07071340c95eb7503b5f47cd5f655e2294a
Lee Youngeung <20eung@gmail.com>
2022-08-15 10:39:25 +0900

    Update vpn.tf

--- modules/bgp/vpn.tf
+++ modules/bgp/vpn.tf
@@ -1,3 +1,4 @@
+```
 terraform {
     required_providers {
         fortios	= {
@@ -7,59 +8,60 @@ terraform {
 }
 
 resource "fortios_vpnipsec_phase1interface" "azure-us-1" {
-  name			        = "${lookup(var.azure_vpns[0], "name")}"
-  interface		      = "wan1"
-  ike_version       = "2"
-  dpd			          = "on-idle"
-  keylife		        = 28800
-  peertype		      = "any"
-  net_device	      = "disable"
-  proposal		      = "aes128-sha1 3des-sha1 aes256-sha256"
-  dhgrp			        = "2"
-  remote_gw		      = "${lookup(var.azure_vpns[0], "remote_gw")}"
-  psksecret		      = "sknetvpn"
+  name			= "${lookup(var.azure_vpns[0], "name")}"
+  interface		= "wan1"
+  ike_version           = "2"
+  dpd			= "on-idle"
+  keylife		= 28800
+  peertype		= "any"
+  net_device	        = "disable"
+  proposal		= "aes128-sha1 3des-sha1 aes256-sha256"
+  dhgrp			= "2"
+  remote_gw		= "${lookup(var.azure_vpns[0], "remote_gw")}"
+  psksecret		= "sknetvpn"
   dpd_retryinterval	= "10"
-  nattraversal		  = "disable"
+  nattraversal		= "disable"
 }
 
 
 # IPsec Phase2-interface 설정
 resource "fortios_vpnipsec_phase2interface" "azure-us-1" {
-  name			        = fortios_vpnipsec_phase1interface.azure-us-1.name
-  phase1name		    = fortios_vpnipsec_phase1interface.azure-us-1.name
-  proposal		      = "aes128-sha1 3des-sha1 aes256-sha256"
-  pfs			          = "disable"
-  auto_negotiate    = "enable"
-  keylifeseconds	  = 27000
+  name			= fortios_vpnipsec_phase1interface.azure-us-1.name
+  phase1name		= fortios_vpnipsec_phase1interface.azure-us-1.name
+  proposal		= "aes128-sha1 3des-sha1 aes256-sha256"
+  pfs			= "disable"
+  auto_negotiate        = "enable"
+  keylifeseconds	= 27000
 
-  depends_on		    = [fortios_vpnipsec_phase1interface.azure-us-1]
+  depends_on		= [fortios_vpnipsec_phase1interface.azure-us-1]
 }
 
 resource "fortios_vpnipsec_phase1interface" "azure-us-2" {
-  name			        = "${lookup(var.azure_vpns[1], "name")}"
-  interface		      = "wan1"
-  ike_version       = "2"
-  dpd			          = "on-idle"
-  keylife		        = 28800
-  peertype		      = "any"
-  net_device	      = "disable"
-  proposal		      = "aes128-sha1 3des-sha1 aes256-sha256"
-  dhgrp			        = "2"
-  remote_gw		      = "${lookup(var.azure_vpns[1], "remote_gw")}"
-  psksecret		      = "sknetvpn"
+  name			= "${lookup(var.azure_vpns[1], "name")}"
+  interface		= "wan1"
+  ike_version           = "2"
+  dpd			= "on-idle"
+  keylife		= 28800
+  peertype		= "any"
+  net_device	        = "disable"
+  proposal		= "aes128-sha1 3des-sha1 aes256-sha256"
+  dhgrp			= "2"
+  remote_gw		= "${lookup(var.azure_vpns[1], "remote_gw")}"
+  psksecret		= "sknetvpn"
   dpd_retryinterval	= "10"
-  nattraversal		  = "disable"
+  nattraversal		= "disable"
 }
 
 
 # IPsec Phase2-interface 설정
 resource "fortios_vpnipsec_phase2interface" "azure-us-2" {
-  name			        = fortios_vpnipsec_phase1interface.azure-us-2.name
-  phase1name		    = fortios_vpnipsec_phase1interface.azure-us-2.name
-  proposal		      = "aes128-sha1 3des-sha1 aes256-sha256"
-  pfs			          = "disable"
-  auto_negotiate    = "enable"
-  keylifeseconds	  = 27000
+  name			= fortios_vpnipsec_phase1interface.azure-us-2.name
+  phase1name		= fortios_vpnipsec_phase1interface.azure-us-2.name
+  proposal		= "aes128-sha1 3des-sha1 aes256-sha256"
+  pfs			= "disable"
+  auto_negotiate        = "enable"
+  keylifeseconds	= 27000
 
-  depends_on		    = [fortios_vpnipsec_phase1interface.azure-us-2]
+  depends_on		= [fortios_vpnipsec_phase1interface.azure-us-2]
 }
+```
